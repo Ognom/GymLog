e713dd9d00d7163b5eb615d1698225668e94f828
Fredrik Hansson <fiddendi@gmail.com>
2013-04-08 11:37:47 +0200

    Removed unnecessary classes and added some TODO's

--- src/com/ognom/gymlog/NewExercise.java
+++ src/com/ognom/gymlog/NewExercise.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,9 @@ public class NewExercise extends Activity implements OnClickListener{
 	Button addExercise;
 	EditText exerciseName, description;
 	AutoCompleteTextView category;
+	
+	//TODO: Dummy, replace with actual exercises from the database
+	String[] exerciseSuggestions = {""};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +31,19 @@ public class NewExercise extends Activity implements OnClickListener{
 		addExercise = (Button) findViewById(R.id.bAddExercise);
 		exerciseName = (EditText) findViewById(R.id.eTExerciseName);
 		description = (EditText) findViewById(R.id.eTDescription);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, exerciseSuggestions);
 		category = (AutoCompleteTextView) findViewById(R.id.aCTVCategory);
+		category.setAdapter(adapter);
 		
 		addExercise.setOnClickListener(this);
 	}
 
 	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
+	public void onClick(View v) {
+		
+		// TODO: addExercise is the only element able to call this function. Add the exercise in the database with corresponding elements.
 		
 	}
 
