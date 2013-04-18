b466aa792e03d9d2bc37ba4d647c075b68342bb8
Fredrik Hansson <fiddendi@gmail.com>
2013-04-18 09:35:56 +0200

    Added a splash screen that's visible 2 seconds during startup.

--- src/com/ognom/gymlog/SplashScreen.java
+++ src/com/ognom/gymlog/SplashScreen.java
@@ -3,49 +3,40 @@ package com.ognom.gymlog;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.Window;
 
-public class SplashScreen extends Activity{
+public class SplashScreen extends Activity {
 	
-    private boolean mIsBackButtonPressed; //Used to know if the back button was pressed in the splash screen activity and avoid opening the next activity
-    private static final int SPLASH_DURATION = 5000; //5 seconds
-
-
-    public void onCreate(Bundle savedInstanceState) {
+    private long ms = 0;
+    private long splashTime = 2000;
+    private boolean splashActive = true;
+    private boolean paused = false;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Hides the title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.splash);
 
-        Handler handler = new Handler();
-
-        //Run a thread after 2 seconds to start the home screen
-        handler.postDelayed(new Runnable() {
-
-            @Override
+        Thread mythread = new Thread() {
             public void run() {
-
-                //Make sure we close the splash screen so the user won't come back when it presses back key
-
-                finish();
-                
-                if (!mIsBackButtonPressed) {
-                    //Start the home screen if the back button wasn't pressed already 
+                try {
+                    while (splashActive && ms < splashTime) {
+                        if(!paused)
+                            ms=ms+100;
+                        sleep(100);
+                    }
+                } 
+                catch(Exception e) {}
+                finally {
                     Intent intent = new Intent(SplashScreen.this, MainMenu.class);
-                    SplashScreen.this.startActivity(intent);
-               }
-                
+                    startActivity(intent);
+                }
             }
-
-        }, SPLASH_DURATION); // time in milliseconds (1 second = 1000 milliseconds) until the run() method will be called
-
-    }
-
-    @Override
-   public void onBackPressed() {
-
-        //Set the flag to true so the next activity won't start up
-        mIsBackButtonPressed = true;
-        super.onBackPressed();
-
+        };
+        mythread.start();
     }
 }
