86e210f8605cdcb2b43f3d840d1445eb63e99237
Fredrik Hansson <fiddendi@gmail.com>
2013-05-16 17:23:59 +0200

    pic of er-diagram added.

--- src/com/ognom/gymlog/database/DbHelper.java
+++ src/com/ognom/gymlog/database/DbHelper.java
@@ -113,7 +113,7 @@ class DbHelper extends SQLiteOpenHelper {
 		ContentValues values = new ContentValues();
 
 		String mydate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime()); //A string representing a date
-																																		  //in the form of May 7, 2013 9:26:03 AM 
+																											 //in the form of May 7, 2013 9:26:03 AM 
 		values.put(DbHelper.C_DATE, mydate);
 		db.insertOrThrow(DbHelper.TABLE2, null, values);
 		
